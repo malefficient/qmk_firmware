4040bc647fcdd0c8907bf0b4ca2cad48a2ae8bdd
Johnny Cache <35432057+malefficient@users.noreply.github.com>
2021-07-21 03:14:51 +0000

    default_two is a slight deviation from johnnycdev. You probably are looking for 2d2

---
+++ keyboards/sofle/keymaps/default_two/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define DEBUG_MATRIX_SCAN_RATE
+#define EE_HANDS
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
---
+++ keyboards/sofle/keymaps/default_two/rules.mk
@@ -0,0 +1,9 @@
+
+OLED_DRIVER_ENABLE = yes
+ENCODER_ENABLE = yes
+CONSOLE_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+WPM_ENABLE = yes
+#// Forcing handedness with
+#// make sofle:default_two:avr-split-left|right seems to work 
